8c076ca72826d07421f2ebe5def3b45175226afc
longuyenothere <longnt2635@ut.edu.vn>
2025-04-21 07:39:12 +0700

    Delete DemoProject directory

--- DemoProject/src/main/java/ut/edu/demoproject/Repositories/DVRepository.java
+++
@@ -1,9 +0,0 @@
-package ut.edu.demoproject.Repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-import ut.edu.demoproject.Model.DV;
-import org.springframework.stereotype.Repository;
-import java.util.*;
-@Repository
-public interface DVRepository extends JpaRepository<DV,Long> {
-    Optional<DV> findByServiceName(String serviceName);
-}
